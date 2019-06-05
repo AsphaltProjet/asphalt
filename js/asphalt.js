c9162135b84e0605fa0713b9e041b851e3ac6606
Pierre29G <49999698+Pierre29G@users.noreply.github.com>
2019-06-05 16:09:37 +0200

    mise en place du swap de texte

--- js/asphalt.js
+++ js/asphalt.js
@@ -1,6 +1,19 @@
 "use strict"; /* oblige à déclarer toute variable utilisée */
 $(document).ready(initialiser);
-function initialiser(evt){}
+function initialiser(evt){
+
+  var flecheR= document.getElementById("flecheSwap");
+  flecheR.addEventListener("click",swapTexte);
+
+}
+
+function swapTexte(evt){
+    var depart    = document.getElementById("lieuDepart");
+    var arrivee   = document.getElementById("lieuArrivee");
+    var swaper    = depart.value;
+    depart.value  = arrivee.value;
+    arrivee.value = swaper;
+}
 
 $('.star').on('change', function() {
   let stars = $(this).val();
