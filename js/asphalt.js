5c58f2775df6a30c55b3f2405fae5d7a5b444bc2
MaudeF <50012003+MaudeF@users.noreply.github.com>
2019-06-05 12:05:33 +0200

    Update asphalt.js

--- js/asphalt.js
+++ js/asphalt.js
@@ -1,3 +1,9 @@
 "use strict"; /* oblige à déclarer toute variable utilisée */
 $(document).ready(initialiser);
 function initialiser(evt){}
+
+$('.star').on('change', function() {
+  let stars = $(this).val();
+  /* Make an AJAX call to register the rating */
+  console.log(stars);
+});
