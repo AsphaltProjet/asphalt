671ea97553eae07091bbb56ba41bc2a332695759
Samuel666666 <49899321+Samuel666666@users.noreply.github.com>
2019-06-06 15:27:17 +0200

    add entete.inc.php

---
+++ entete.inc.php
@@ -0,0 +1,43 @@
+<?php 
+  header("Content-Type: text/html; charset=utf-8") ;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>ASPHALT</title>
+    <meta name="description" content="??????">
+    <link rel="shortcut icon" href="images/logo.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Passion+One" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.js'></script>
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
+
+ </head>
+    
+    
+    
+<body>
+    
+
+<header>
+    <div>
+        <a href="index.html" title="page d'accueil"><img src="images/logoAsphalt.png" alt="logo d'Asphalt" /></a>
+        <a href="index.html" title="page d'accueil">
+            <h1>ASPHALT</h1>
+        </a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="rechercheTrajet.html">Rechercher</a></li>
+            <li class="pageActuel"><a href="proposerTrajet.html">Proposer</a></li>
+            <li><a href="inscription.html">Inscription</a></li>
+            <li><a href="connexion.html">Connexion</a></li>
+        </ul>
+    </nav>
+</header>
+    
+   
