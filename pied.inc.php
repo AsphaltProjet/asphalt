8cd1eba4287adbce3ac72828efadf7e600a2ab6b
Samuel666666 <49899321+Samuel666666@users.noreply.github.com>
2019-06-06 15:30:35 +0200

    add pied.inc.php

---
+++ pied.inc.php
@@ -0,0 +1,13 @@
+    <footer>
+        <div class="lienFoot">
+        <a href= "aPropos.html"><p>À propos</p></a>
+        <a href="nousContacter.html"><p>Nous contacter</p></a>
+        <a href="CGU.html"><p>CGU- confidentialité</p></a>
+        <a href="#"><p><i class="fab fa-instagram"></i></p></a>
+        <a href="#"><p><i class="fab fa-twitter-square"></i></p></a>
+        <a href="#"><p><i class="fab fa-facebook-square"></i></p></a>
+        </div>
+    </footer>
+</body>
+    
+</html>
