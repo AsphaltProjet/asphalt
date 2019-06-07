461d489f07109b5aa6e19c736af3c48b793142c8
coralinepqt <49899092+coralinepqt@users.noreply.github.com>
2019-06-07 09:19:49 +0200

    création page inscription php

---
+++ inscription.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+require_once ('param.inc.php');
+
+echo('1');
+try{
+	$pdo=new PDO("mysql:host=".MYHOST.";dbname=".MYDB."", MYUSER, MYPASS);
+    $pdo->query("SET NAMES utf8");
+    $pdo->query("SET CHARACTER SET 'utf8'");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (Exception $e){
+	die("Un problème est survenu lors de votre inscription : ".$e->getMessage());
+}
+
+// requete pour ajouter membre
+$ajouterMembre=$pdo->prepare('INSERT INTO USER(nomUser, prenomUser, dateNaissanceUser, mailUser, motDePasseUser, numTel, urlPP,statutUser, nbTrajets, cagnotte) VALUES(:nom, :prenom, :dateNaissance, :mail, :mdp, :num, :photo, :statut, :nbTrajet, :cagnotte)');
+
+$existMail=$pdo->prepare('SELECT idUser FROM USER WHERE mailUser=?');
+
+
+if (isset($_POST['nom']) && 
+    isset($_POST['prenom']) &&
+    isset($_POST['adresseMail']) &&
+    isset($_POST['numTel']) &&
+    isset($_POST['dateNaissance']) &&
+    isset($_POST['genre']) &&
+    isset($_POST['mdp']) &&
+    isset($_POST['confirmationMdp']) &&
+    isset($_POST['conditionsUtilisation'])&&
+    ($_POST['conducteur']==true || $_POST['passager']==true)){
+    
+echo('3');
+    if($_POST['mdp']==$_POST['confirmationMdp']){
+        
+        $existMail->execute(array($_POST['mailUser']));
+        $testMail=$existMail->fetch();
+        if(!isset($testMail['idUser'])){ //verif si adresse mail existe pas deja
+            
+            $hashPass="des".$_POST['mdp']."sapins";
+            $hashPass=hash('sha512',$hashPass);
+            $ajouterMembre->execute(array(
+                'nom'=> $_POST['nom'],
+                'prenom'=> $_POST['prenom'],
+                'dateNaissance'=> $_POST['dateNaissance'],
+                'mail'=> $_POST['adresseMail'],
+                'mdp'=>$hashPass,
+                'num'=>$_POST['numTel'],
+                'photo'=>'avatar.png',
+                'statut'=>($_POST['conducteur'].",".$_POST['passager']),
+                'nbTrajet'=>0, 
+                'cagnotte'=>0
+            )) or die(print_r($pdo->errorInfo()));
+            header('Location: confirmationInscription.html');
+            
+        } else{
+            echo ('l\'email est deja utilisé');
+        }
+        
+    } else{
+        echo('Tu dois indiquer le même mot de passe');
+    }
+}else{
+    echo('Il manque des informations');
+}
+
+$existMail->closeCursor();
+
+
+?>
+
+
+
+
+ 
