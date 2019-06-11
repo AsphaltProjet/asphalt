1413191b7602ec977685f07f3cfb54273605b3bb
Pierre29G <49999698+Pierre29G@users.noreply.github.com>
2019-06-11 17:00:23 +0200

    changement destination nav

--- entete.inc.php
+++ entete.inc.php
@@ -1,29 +1,4 @@
-<?php 
-  header("Content-Type: text/html; charset=utf-8") ;
-?>
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="utf-8">
-    <title>ASPHALT</title>
-    <meta name="description" content="??????">
-    <link rel="shortcut icon" href="images/logo.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="css/style.css" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i,900,900i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Passion+One" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-        <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.js'></script>
-    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
-
- </head>
-    
-    
-    
-<body>
-    
-
-<header>
+    <header>
     <div>
         <a href="index.html" title="page d'accueil"><img src="images/logoAsphalt.png" alt="logo d'Asphalt" /></a>
         <a href="index.html" title="page d'accueil">
@@ -31,10 +6,10 @@
         </a>
     </div>
         <ul class="navPC">
-            <li><a href="rechercheTrajet.html">Rechercher</a></li>
-            <li><a href="proposerTrajet.html">Proposer</a></li>
+            <li><a href="rechercheTrajet.php">Rechercher</a></li>
+            <li><a href="proposerTrajet.php">Proposer</a></li>
             <li><a href="inscription.html">Inscription</a></li>
-            <li><a href="connexion.html">Connexion</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
         </ul>
 
 <input id="burger" type="checkbox" />
@@ -47,15 +22,13 @@
 
 <nav>    
   <ul>
-    <li><a href="rechercheTrajet.html">Rechercher</a></li>
-    <li><a href="proposerTrajet.html">Proposer</a></li>
+    <li><a href="rechercheTrajet.php">Rechercher</a></li>
+    <li><a href="proposerTrajet.php">Proposer</a></li>
     <li><a href="inscription.html">Inscription</a></li>
-    <li><a href="connexion.html">Connexion</a></li>
-    <li><a href="aPropos.html">À propos</a></li>
-    <li><a href="nousContacter.html">Nous contacter</a></li>
-    <li><a href="CGU.html">CGU- confidentialité</a></li>
+    <li><a href="connexion.php">Connexion</a></li>
+    <li><a href="aPropos.php">À propos</a></li>
+    <li><a href="nousContacter.php">Nous contacter</a></li>
+    <li><a href="CGU.php">CGU- confidentialité</a></li>
   </ul>  
     </nav>
     </header>
-    
-   
